3c1a90d1ede2936fe8eb827d00e2c1d4ee57439d
kgneteu <77528950+kgneteu@users.noreply.github.com>
2021-10-16 23:35:27 +0200

    Updates

---
+++ precache-manifest.33132009681e7111990329a34c9ccec5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b93d0220e1706c62b0297176256ee5d",
+    "url": "/react-robofriends-redux/index.html"
+  },
+  {
+    "revision": "05dc07faba8c174bbd68",
+    "url": "/react-robofriends-redux/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "e76a8bcba27d08b1c7a1",
+    "url": "/react-robofriends-redux/static/css/main.17cfbac0.chunk.css"
+  },
+  {
+    "revision": "05dc07faba8c174bbd68",
+    "url": "/react-robofriends-redux/static/js/2.a6f0891f.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/react-robofriends-redux/static/js/2.a6f0891f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e76a8bcba27d08b1c7a1",
+    "url": "/react-robofriends-redux/static/js/main.f7123731.chunk.js"
+  },
+  {
+    "revision": "3ff80ee28ac140faa420",
+    "url": "/react-robofriends-redux/static/js/runtime-main.fa872842.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robofriends-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
