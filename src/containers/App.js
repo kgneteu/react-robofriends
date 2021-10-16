4ab6eed0511c5e6a0635b2329a61d30dc1f6fa78
kgneteu <77528950+kgneteu@users.noreply.github.com>
2021-10-17 00:54:54 +0200

    conversion to hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,57 +1,49 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { setSearchField, requestRobots } from '../actions';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {requestRobots, setSearchField} from '../actions';
 
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 
 import './App.css';
 
 // parameter state comes from index.js provider store state(rootReducers)
-const mapStateToProps = (state) => {
-  return {
-    searchField: state.searchRobots.searchField,
-    robots: state.requestRobots.robots,
-    isPending: state.requestRobots.isPending
-  }
-}
+// const mapStateToProps = (state) => {
+//     return {
+//         searchField: state.searchRobots.searchField,
+//         robots: state.requestRobots.robots,
+//         isPending: state.requestRobots.isPending
+//     }
+// }
 
-// dispatch the DOM changes to call an action. note mapStateToProps returns object, mapDispatchToProps returns function
-// the function returns an object then uses connect to change the data from redecers.
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-    onRequestRobots: () => dispatch(requestRobots())
-  }
-}
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onRequestRobots();
-  }
+function App() {
 
-  render() {
-    const { robots, searchField, onSearchChange, isPending } = this.props;
+    const {searchField,} = useSelector(state => state.searchRobots)
+    const {robots, isPending} = useSelector(state => state.requestRobots)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(requestRobots());
+    }, [dispatch])
+
     const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
+
     return (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={onSearchChange}/>
-        <Scroll>
-          { isPending ? <h1>Loading</h1> :
-            <ErrorBoundry>
-              <CardList robots={filteredRobots} />
-            </ErrorBoundry>
-          }
-        </Scroll>
-      </div>
+        <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange={(event) => dispatch(setSearchField(event.target.value))}/>
+            {isPending ? <h1>Loading</h1> :
+                <ErrorBoundry>
+                    <CardList robots={filteredRobots}/>
+                </ErrorBoundry>
+            }
+        </div>
     );
-  }
+
 }
 
-// action done from mapDispatchToProps will channge state from mapStateToProps
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App; //connect(mapStateToProps, mapDispatchToProps)(App)
