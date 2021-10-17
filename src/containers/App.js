eaf9745c92a68c3a5b26aea5b9c0b0366ec89fb6
kgneteu <77528950+kgneteu@users.noreply.github.com>
2021-10-17 02:12:51 +0200

    fixed header

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {requestRobots, setSearchField} from '../actions';
 
@@ -14,10 +14,22 @@ function App() {
     const {searchField,} = useSelector(state => state.searchRobots)
     const {robots, isPending} = useSelector(state => state.requestRobots)
     const dispatch = useDispatch();
+    const [scrolled, setScrolled] = useState(false);
+
+    const handleScroll = () => {
+        setScrolled((window.scrollY > 8) && !scrolled)
+    }
 
     useEffect(() => {
         dispatch(requestRobots());
     }, [dispatch])
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll)
+        return () => {
+            window.removeEventListener("scroll", () => handleScroll)
+        }
+    }, [])
 
     const filteredRobots = robots.filter(robot => {
         return robot.name.toLowerCase().includes(searchField.toLowerCase());
@@ -25,7 +37,16 @@ function App() {
 
     return (
         <>
-            <header style={{position: 'fixed', top: 0, textAlign: 'center', width: '100%', height: '200px', zIndex: '100'}}>
+            <header
+                style={{position: 'fixed',
+                    top: 0,
+                    textAlign: 'center',
+                    width: '100%',
+                    height: '200px',
+                    zIndex: '100',
+                    boxShadow: scrolled ? '0 4px 4px rgba(0, 0, 0, 0.4)': 'none',
+                    transition: 'all 0.2s ease-out',
+                }}>
                 <h1 className='f1 center'>RoboFriends</h1>
                 <SearchBox className={'ml-auto'}
                            searchChange={(event) => dispatch(setSearchField(event.target.value))}/>
