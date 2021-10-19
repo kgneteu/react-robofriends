933625ed7af321acc4215ccd9bb5d9fbc3d51288
kgneteu <77528950+kgneteu@users.noreply.github.com>
2021-10-19 20:22:40 +0200

    Updates

--- precache-manifest.7cd3a9c7b5a6e4954780fe621e86025b.js
+++ precache-manifest.7cd3a9c7b5a6e4954780fe621e86025b.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d908086ce2028624270e3ff2f8a4d58c",
+    "revision": "bf4e09d3f077def2e5fa36a95e11b43a",
     "url": "/react-robofriends-redux/index.html"
   },
   {
-    "revision": "c91d922f90aa1bae5219",
+    "revision": "7d17c4bf97ad29aa59d8",
     "url": "/react-robofriends-redux/static/css/2.3b2cfc2e.chunk.css"
   },
   {
-    "revision": "925ff808845b29247eff",
+    "revision": "01491fa876097f183643",
     "url": "/react-robofriends-redux/static/css/main.a6a6a69e.chunk.css"
   },
   {
-    "revision": "c91d922f90aa1bae5219",
-    "url": "/react-robofriends-redux/static/js/2.0ab1607f.chunk.js"
+    "revision": "7d17c4bf97ad29aa59d8",
+    "url": "/react-robofriends-redux/static/js/2.afc5b464.chunk.js"
   },
   {
     "revision": "f7e623707d9b37a3821ec4c3548a4309",
-    "url": "/react-robofriends-redux/static/js/2.0ab1607f.chunk.js.LICENSE.txt"
+    "url": "/react-robofriends-redux/static/js/2.afc5b464.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "925ff808845b29247eff",
-    "url": "/react-robofriends-redux/static/js/main.dbdb48f9.chunk.js"
+    "revision": "01491fa876097f183643",
+    "url": "/react-robofriends-redux/static/js/main.91311001.chunk.js"
   },
   {
     "revision": "3ff80ee28ac140faa420",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-robofriends-redux/precache-manifest.38990ddec71ff10e4b9e35059c88e9a6.js"
+  "/react-robofriends-redux/precache-manifest.7cd3a9c7b5a6e4954780fe621e86025b.js"
 );
 
 self.addEventListener('message', (event) => {
